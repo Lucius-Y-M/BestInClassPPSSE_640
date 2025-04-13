b66413f0ac4f19ff16fb205ce163d4482a3d63df
SeaSparrow <SeaSparrowNexus@gmail.com>
2025-04-13 10:41:51 +0300

    Different logic

--- src/ItemVisitor/ItemVisitor.cpp
+++ src/ItemVisitor/ItemVisitor.cpp
@@ -2,188 +2,245 @@
 
 namespace ItemVisitor
 {
-	ItemListVisitor::ItemListVisitor(const RE::BSTArray<RE::ItemList::Item*> a_itemList) :
-		_list(a_itemList)
-	{
+	ItemListVisitor::ItemListVisitor(const RE::BSTArray<RE::ItemList::Item*> a_itemList) {
+		_list = a_itemList;
+
+		auto* gamePlayer = RE::PlayerCharacter::GetSingleton();
+		if (!gamePlayer) {
+			throw new std::exception("ItemListVisitor: Failed to cache player.");
+		}
+		player = gamePlayer;
+
+		auto* dobj = RE::BGSDefaultObjectManager::GetSingleton();
+		if (!dobj) {
+			throw new std::exception("ItemListVisitor: Failed to cache dobj.");
+		}
+		auto* eitherSlot = dobj->GetObject<RE::BGSEquipSlot>(RE::DEFAULT_OBJECT::kEitherHandEquip);
+		if (!eitherSlot) {
+			throw new std::exception("ItemListVisitor: Failed to cache either slot.");
+		}
+		either = eitherSlot;
+
+		auto* headKeyword = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("ArmorHelmet"sv);
+		if (!headKeyword) {
+			throw new std::exception("ItemListVisitor: Failed to cache head keyword.");
+		}
+		auto* cuirassKeyword = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("ArmorCuirass"sv);
+		if (!cuirassKeyword) {
+			throw new std::exception("ItemListVisitor: Failed to cache body keyword.");
+		}
+		auto* armsKeyword = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("ArmorGauntlets"sv);
+		if (!armsKeyword) {
+			throw new std::exception("ItemListVisitor: Failed to cache arms keyword.");
+		}
+		auto* bootsKeyword = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("ArmorBoots"sv);
+		if (!bootsKeyword) {
+			throw new std::exception("ItemListVisitor: Failed to cache boots keyword.");
+		}
+		wornCuirass = headKeyword;
+		wornHelmet = cuirassKeyword;
+		wornArms = armsKeyword;
+		wornLegs = bootsKeyword;
+
+		auto* heavyKeyword = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("ArmorHeavy"sv);
+		if (!heavyKeyword) {
+			throw new std::exception("ItemListVisitor: Failed to cache heavy keyword.");
+		}
+		auto* lightKeyword = RE::TESForm::LookupByEditorID<RE::BGSKeyword>("ArmorLight"sv);
+		if (!lightKeyword) {
+			throw new std::exception("ItemListVisitor: Failed to cache light keyword.");
+		}
+		heavyArmor = heavyKeyword;
+		lightArmor = lightKeyword;
 	}
 
-
-	void ItemListVisitor::Run()
-	{
+	void ItemListVisitor::Run() {
 		Visit();
 	}
 
-
-	void ItemListVisitor::Dispose()
-	{
+	void ItemListVisitor::Dispose() {
 		delete this;
 	}
 
-
-	void ItemListVisitor::Visit()
-	{
+	void ItemListVisitor::Visit() {
 		if (!_list.data()) {
 			return;
 		}
 
-		for (auto item : _list) {
-			if (!item) {
+		for (auto* item : _list) {
+			auto* extraData = item ? item->data.objDesc : nullptr;
+			auto* baseObject = extraData ? extraData->GetObject() : nullptr;
+			auto formType = baseObject ? baseObject->GetFormType() : RE::FormType::None;
+
+			if (!baseObject || !baseObject->GetPlayable()) {
 				continue;
 			}
 
-			switch (item->data.objDesc->GetObject()->GetFormType()) {
-			case RE::FormType::Armor:
-			{
-				CompareArmor(item);
-				break;
-			}
-			case RE::FormType::Weapon:
-			{
-				CompareWeapon(item);
-				break;
-			}
+			item->obj.SetMember("bestInClass", false);
+			bool skip = false;
+
+			RE::TESAmmo* ammo = nullptr;
+			RE::TESObjectWEAP* weap = nullptr;
+			RE::TESObjectARMO* armo = nullptr;
+
+			switch (formType) {
 			case RE::FormType::Ammo:
-			{
-				CompareAmmo(item);
+				ammo = baseObject->As<RE::TESAmmo>();
+				break;
+			case RE::FormType::Armor:
+				armo = baseObject->As<RE::TESObjectARMO>();
+				break;
+			case RE::FormType::Weapon:
+				weap = baseObject->As<RE::TESObjectWEAP>();
+				break;
+			default:
+				skip = true;
 				break;
 			}
-			default:;
+			if (skip) {
+				continue;
+			}
+
+			if (ammo) {
+				EvaluateAmmo(ammo, extraData, item);
+			}
+			else if (weap) {
+				EvaluateWeapon(weap, extraData, item);
+			}
+			else if (armo) {
+				EvaluateArmor(armo, extraData, item);
 			}
 		}
 
 		SetBest();
 	}
 
-
-	void ItemListVisitor::CompareArmor(RE::ItemList::Item* a_item)
-	{
-		auto* const armor = static_cast<RE::TESObjectARMO*>(a_item->data.objDesc->GetObject());
-		const auto type = std::underlying_type_t<RE::BIPED_MODEL::ArmorType>(armor->GetArmorType()) * 5;
-		const auto slotMask = std::underlying_type_t<RE::BIPED_MODEL::BipedObjectSlot>(armor->GetSlotMask());
-		uint32_t typeMask;
-
-		switch (UnmaskToLowest(slotMask)) {
-		case Slot::kHelmet:
-		{
-			typeMask = type + 0;
-			break;
-		}
-		case Slot::kBody:
-		{
-			typeMask = type + 1;
-			break;
-		}
-		case Slot::kHand:
-		{
-			typeMask = type + 2;
-			break;
-		}
-		case Slot::kFeet:
-		{
-			typeMask = type + 3;
-			break;
-		}
-		case Slot::kShield:
-		{
-			typeMask = type + 4;
-			break;
-		}
-		default:
-		{
-			typeMask = -1;
-			break;
-		}
-		}
-
-		// discard the rest and ClothingShield, because there's no ClothingShield
-		if (typeMask >= 0 && typeMask < 14) {
-			auto& [item, value] = _bestStore.Armor[typeMask];
-
-			// compare armor by rating, compare clothing by amount of enchantments / maybe gold value?
-			const auto rhsCompare = typeMask < 10 ? armor->GetArmorRating() : armor->amountofEnchantment;
-
-			if (value < rhsCompare) {
-				item = a_item;
-				value = rhsCompare;
+	void ItemListVisitor::SetBest() {
+		for (uint64_t i = 0; i < 4; ++i) {
+			auto& candidate = best.at(i);
+			if (!candidate.item) {
+				continue;
 			}
+
+			uint64_t secondary = i;
+			auto* armor = candidate.item->data.objDesc->GetObject()->As<RE::TESObjectARMO>();
+			if (armor->HasKeyword(heavyArmor->formID)) {
+				secondary += ARMOR_HEAVY_START;
+			}
+			else if (armor->HasKeyword(lightArmor->formID)) {
+				secondary += ARMOR_LIGHT_START;
+			}
+			else {
+				secondary += ARMOR_CLOTH_START;
+			}
+
+			auto& alternate = best.at(secondary);
+			alternate.Compare(candidate.item, candidate.value);
+			alternate.item->obj.SetMember("bestInClass", true);
+		}
+
+		for (uint64_t i = WEAPON_START_INDEX; i < WEAPON_UNEQUIPPED_START + WEAPON_START_INDEX; ++i) {
+			auto& candidate = best.at(i);
+			if (!candidate.item) {
+				continue;
+			}
+
+			uint64_t secondary = i + WEAPON_UNEQUIPPED_START;
+			auto& alternate = best.at(secondary);
+			alternate.Compare(candidate.item, candidate.value);
+			alternate.item->obj.SetMember("bestInClass", true);
+		}
+
+		for (uint64_t i = AMMO_START_INDEX; i < AMMO_START_INDEX + AMMO_UNEQUIPPED_START; ++i) {
+			auto& candidate = best.at(i);
+			if (!candidate.item) {
+				continue;
+			}
+
+			uint64_t secondary = i + AMMO_UNEQUIPPED_START;
+			auto& alternate = best.at(secondary);
+			alternate.Compare(candidate.item, candidate.value);
+			alternate.item->obj.SetMember("bestInClass", true);
 		}
 	}
 
+	void ItemListVisitor::EvaluateArmor(RE::TESObjectARMO* a_armor,
+		RE::InventoryEntryData* a_data,
+		RE::ItemList::Item* a_item) {
+		uint64_t index = 0;
+		bool worn = a_data->IsWorn();
 
-	void ItemListVisitor::CompareWeapon(RE::ItemList::Item* a_item)
-	{
-		auto* const weapon = static_cast<RE::TESObjectWEAP*>(a_item->data.objDesc->GetObject());
-		const auto typeMask = std::underlying_type_t<RE::WEAPON_TYPE>(weapon->GetWeaponType());
+		if (a_armor->HasKeyword(heavyArmor->formID)) {
+			index += worn ? 0 : ARMOR_HEAVY_START;
+		}
+		else if (a_armor->HasKeyword(lightArmor->formID)) {
+			index += worn ? 0 : ARMOR_LIGHT_START;
+		}
+		else if (a_armor->HasKeyword(clothArmor)) {
+			index += worn ? 0 : ARMOR_CLOTH_START;
+		}
+		else {
+			return;
+		}
 
-		auto& [item, value] = _bestStore.Weapon[typeMask];
+		if (a_armor->HasKeyword(wornHelmet->formID)) {
+			index += ARMOR_HEAD_INDEX;
+		}
+		else if (a_armor->HasKeyword(wornArms->formID)) {
+			index += ARMOR_ARMS_INDEX;
+		}
+		else if (a_armor->HasKeyword(wornLegs->formID)) {
+			index += ARMOR_BOOTS_INDEX;
+		}
+		else if (!a_armor->HasKeyword(wornCuirass->formID)) {
+			return;
+		}
 
-		// compare weapon by damage, comapre staff by gold value
-		const auto rhsCompare = typeMask == 8 ? weapon->GetGoldValue() : weapon->GetAttackDamage();
-
-		if (value < rhsCompare) {
-			item = a_item;
-			value = rhsCompare;
+		float value = player->GetArmorValue(a_data);
+		try {
+			best.at(index).Compare(a_item, value);
+		}
+		catch (std::out_of_range& e) {
+			logger::error("Bad index created for {} ({}).", a_armor->GetName(), index);
 		}
 	}
 
-
-	void ItemListVisitor::CompareAmmo(RE::ItemList::Item* a_item)
-	{
-		auto* const ammo = static_cast<RE::TESAmmo*>(a_item->data.objDesc->GetObject());
-		const auto typeMask = ammo->IsBolt() ? 0 : 1;
-
-		auto& [item, value] = _bestStore.Ammo[typeMask];
-
-		// compare ammo by damage
-		const auto rhsCompare = ammo->data.damage;
-
-		if (value < rhsCompare) {
-			item = a_item;
-			value = rhsCompare;
+	void ItemListVisitor::EvaluateWeapon(RE::TESObjectWEAP* a_weap,
+		RE::InventoryEntryData* a_data,
+		RE::ItemList::Item* a_item) {
+		uint64_t index = WEAPON_START_INDEX;
+		auto* slot = a_weap->GetEquipSlot();
+		if (!slot) {
+			return;
 		}
+
+		index += a_weap->IsRanged() ?
+			WEAPON_RANGED_INDEX :
+			slot == either ?
+			WEAPON_ONEHANDED_INDEX :
+			WEAPON_TWOHANDED_INDEX;
+
+		index += a_data->IsWorn() ? 0 : WEAPON_UNEQUIPPED_START;
+
+		float value = player->GetDamage(a_data);
+		best.at(index).Compare(a_item, value);
 	}
 
+	void ItemListVisitor::EvaluateAmmo(RE::TESAmmo* a_weap,
+		RE::InventoryEntryData* a_data,
+		RE::ItemList::Item* a_item) {
+		uint64_t index = AMMO_START_INDEX;
 
-	auto ItemListVisitor::UnmaskToLowest(uint32_t a_slot)
-		-> Slot
-	{
-		// maybe reserve a certain size to increase performance -unbenchmarked
-		std::vector<uint32_t> mask;
+		index += a_data->IsWorn() ? 0 : AMMO_UNEQUIPPED_START;
+		index += a_weap->IsBolt() ? 0 : 1;
 
-		while (a_slot > 0) {
-			mask.push_back(a_slot % 2);
-			a_slot = a_slot / 2;
+		float value = a_weap->data.damage;
+		auto* proj = a_weap->data.projectile;
+		auto* ench = proj ? proj->data.explosionType : nullptr;
+		if (ench) {
+			value += ench->data.damage;
 		}
 
-		for (auto i = 0; i < mask.size(); i++) {
-			// return the lowest (the first) non-zero mask
-			if (mask[i] == 1 && i != 0) {
-				return static_cast<Slot>(i);
-			}
-		}
-
-		return static_cast<Slot>(0);
-	}
-
-
-	void ItemListVisitor::SetBest()
-	{
-		for (auto [item, value] : _bestStore.Armor) {
-			if (item) {
-				item->obj.SetMember("bestInClass", true);
-			}
-		}
-
-		for (auto [item, value] : _bestStore.Weapon) {
-			if (item) {
-				item->obj.SetMember("bestInClass", true);
-			}
-		}
-
-		for (auto [item, value] : _bestStore.Ammo) {
-			if (item) {
-				item->obj.SetMember("bestInClass", true);
-			}
-		}
+		best.at(index).Compare(a_item, value);
 	}
 }
